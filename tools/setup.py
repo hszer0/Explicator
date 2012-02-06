90ddbaa2922e7c2be53f79f58fcbb5ebe9d0479e
hszer0 <hszer0@gmail.com>
2012-02-06 15:02:12 +0100

    New folder structure

---
+++ tools/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Explicator',
+    version='0.1.1',
+    author='Patrick Liem',
+    author_email='hszer0@gmail.com',
+    packages=['tools'],
+    package_data={'tools'},
+    scripts=['main.py'])
