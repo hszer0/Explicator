04554e0e76ef4eab63dbff365f107a031279328c
hszer0 <hszer0@gmail.com>
2012-04-10 10:19:31 +0200

    Added GPL

--- explicator.py
+++ explicator.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+
+#Copyright 2011-2012 Patrick Liem
+#
+#This file is part of Explicator.
+#
+#Explicator is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Explicator is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Explicator.  If not, see <http://www.gnu.org/licenses/>.
+
 import DBConnection
 import dialog
 from globals import *
--- globals.py
+++ globals.py
@@ -1,3 +1,20 @@
+#Copyright 2011-2012 Patrick Liem
+#
+#This file is part of Explicator.
+#
+#Explicator is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Explicator is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Explicator.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 statuslist = 'active', 'available', 'on hold', 'canceled', 'completed'
