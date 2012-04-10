953107522ee27a15ccfbc10f8d938510152a15a1
hszer0 <hszer0@gmail.com>
2012-04-10 14:46:20 +0200

    Added LGPL

--- explicator.py
+++ explicator.py
@@ -5,16 +5,16 @@
 #This file is part of Explicator.
 #
 #Explicator is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
+#it under the terms of the GNU Lesser General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
 #
 #Explicator is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+#GNU Lesser General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
+#You should have received a copy of the GNU Lesser General Public License
 #along with Explicator.  If not, see <http://www.gnu.org/licenses/>.
 
 import DBConnection
@@ -481,6 +481,7 @@ class MyDotWindow(xdot.DotWindow):
 
     def refresh_graph(self, dotcode):
         window.set_dotcode(dotcode)
+        print dotcode
 
     def on_status_change_task(self, combobox):
         if self.tid is not None:
